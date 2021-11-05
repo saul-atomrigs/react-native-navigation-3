b5b49b2efa13f319ebe4d44c0930a6a8453b66d8
solleedata <solleedata@gmail.com>
2021-11-05 19:17:44 +0900

    feat: react navigation setup

--- App.js
+++ App.js
@@ -1,25 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>hello world</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-planet'
+                : 'ios-planet-outline';
+            } else if (route.name === 'Community') {
+              iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+            } else if (route.name === 'Calendar') {
+              iconName = focused ? 'calendar' : 'calendar-outline';
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} />
+        <Tab.Screen name="Community" component={CommunityScreen} options={{ tabBarBadge: 5 }} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen({ navigation }) {
+  return (
+    <View style={center}>
+      <Text>Home!</Text>
+      <Button title="Go to calendar" onPress={() => navigation.navigate('Calendar')}
+      />
+    </View>
+  );
+}
+
+function CommunityScreen() {
+  return (
+    <View style={center}>
+      <Text>Community!</Text>
+    </View>
+  );
+}
+
+function CalendarScreen() {
+  return (
+    <View style={center}>
+      <Text>Calendar!</Text>
+    </View>
+  );
+}
+
+
+// react navigation
+const Tab = createBottomTabNavigator();
+
+
+// styling 
+const grayBox = {
+  boxSizing: "border-box",
+  flexShrink: 0,
+  width: 80,
+  height: 80,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  padding: "0px 13px 0px 13px",
+  backgroundColor: "#e6e6e6",
+  overflow: "visible",
+  borderRadius: 5,
+}
+
+const center = {
+  flex: 1, justifyContent: 'center', alignItems: 'center'
+}
