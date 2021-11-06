470d0a54fd26be468cb5e6bb64f36b1116cc625d
solleedata <solleedata@gmail.com>
2021-11-06 11:28:17 +0900

    feat: header options

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 
@@ -13,15 +14,12 @@ export default function App() {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-planet'
-                : 'ios-planet-outline';
+              iconName = focused ? 'ios-planet' : 'ios-planet-outline';
             } else if (route.name === 'Community') {
               iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
             } else if (route.name === 'Calendar') {
               iconName = focused ? 'calendar' : 'calendar-outline';
             }
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: 'black',
@@ -31,11 +29,13 @@ export default function App() {
         <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} />
         <Tab.Screen name="Community" component={CommunityScreen} options={{ tabBarBadge: 5 }} />
         <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="HeaderButton" component={StackScreen} options={{ tabBarBadge: 3 }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
+// Home Screen
 function HomeScreen({ navigation }) {
   return (
     <View style={center}>
@@ -46,6 +46,7 @@ function HomeScreen({ navigation }) {
   );
 }
 
+// Community Screen 
 function CommunityScreen() {
   return (
     <View style={center}>
@@ -54,6 +55,7 @@ function CommunityScreen() {
   );
 }
 
+// Schedules, birthdays, etc. Screen
 function CalendarScreen() {
   return (
     <View style={center}>
@@ -62,9 +64,21 @@ function CalendarScreen() {
   );
 }
 
+// Test Screen with header 
+function StackScreen() {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="KPOP"
+        component={HomeScreen}
+      />
+    </Stack.Navigator>
+  );
+}
 
 // react navigation
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 
 // styling 
@@ -82,7 +96,19 @@ const grayBox = {
   overflow: "visible",
   borderRadius: 5,
 }
-
 const center = {
   flex: 1, justifyContent: 'center', alignItems: 'center'
+}
+const screenOptions = {
+  headerTitle: props => <Text {...props} />,
+  headerStyle: {
+    backgroundColor: '#e6e6e6',
+  },
+  headerRight: () => (
+    <Button
+      onPress={() => alert('This is a button!')}
+      title="Info"
+      color="#000000"
+    />
+  ),
 }
