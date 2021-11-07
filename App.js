5c6880be04b0005f315f29d47db40438493a3504
solleedata <solleedata@gmail.com>
2021-11-07 09:12:32 +0900

    feat, style: GiftedChat initial setup

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react';
 import { Text, View, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 export default function App() {
@@ -121,10 +122,44 @@ function LikeCounter() {
       <Button
         title='Give us feedback'
       />
+      <Chat style={{ width: '100%' }} />
     </View>
   );
 }
+// Gifted Chat 
+export function Chat() {
+  const [messages, setMessages] = useState([]);
 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+  return (
+    <GiftedChat
+      style={{ width: '100%' }}
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
+}
 
 // react navigation
 const Tab = createBottomTabNavigator();
