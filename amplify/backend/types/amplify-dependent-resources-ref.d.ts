3d7b23e46bd99c37b3945b41ab11c1a465722568
solleedata <solleedata@gmail.com>
2021-12-27 22:10:10 +0900

    fix: aws cloudformation stacks

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
-        "dailykpopserver": {
+        "dailykpop": {
             "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "dailykpopserverd0964ef5": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
         }
     }
 }
